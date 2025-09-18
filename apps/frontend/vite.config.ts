d9235245faff219d8047cc4d2821046afa709f9c
Pedro Gonçalves <1986200+mapherez@users.noreply.github.com>
2025-09-18 01:03:08 +0100

    fix configs

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { qrcode } from 'vite-plugin-qrcode'
 
 const backend = process.env.VITE_API_BASE || "http://localhost:8080";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), qrcode()],
   server: {
-    host: "127.0.0.1",
+    host: true,
     strictPort: true,
     port: 5174,
     proxy: {
